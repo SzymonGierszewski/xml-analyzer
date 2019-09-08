c8b91d72beef21b18b148a3d85f66b605053e77f
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-09-08 15:22:26 +0200

    add interface XmlDetailsService w/ method createXmlDetails

---
+++ src/main/java/com/szymongierszewski/xmlanalyzer/service/XmlDetailsService.java
@@ -0,0 +1,10 @@
+package com.szymongierszewski.xmlanalyzer.service;
+
+import com.szymongierszewski.xmlanalyzer.model.XmlDetails;
+
+import javax.xml.stream.XMLStreamReader;
+
+public interface XmlDetailsService {
+
+    XmlDetails createXmlDetails(XMLStreamReader xmlStreamReader);
+}
