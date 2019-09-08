7e63c6e81c51208c450b165ac07b86bd9d8b004b
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-09-08 15:02:52 +0200

    add PostsDetails.java

---
+++ src/main/java/com/szymongierszewski/xmlanalyzer/model/PostsDetails.java
@@ -0,0 +1,52 @@
+package com.szymongierszewski.xmlanalyzer.model;
+
+import java.time.LocalDateTime;
+
+public class PostsDetails {
+
+    private LocalDateTime firstPostDateTime;
+    private LocalDateTime lastPostDateTime;
+    private Integer totalPosts;
+    private Integer totalAcceptedPosts;
+    private Double postsAvgScore;
+
+    public LocalDateTime getFirstPostDateTime() {
+        return firstPostDateTime;
+    }
+
+    public void setFirstPostDateTime(LocalDateTime firstPostDateTime) {
+        this.firstPostDateTime = firstPostDateTime;
+    }
+
+    public LocalDateTime getLastPostDateTime() {
+        return lastPostDateTime;
+    }
+
+    public void setLastPostDateTime(LocalDateTime lastPostDateTime) {
+        this.lastPostDateTime = lastPostDateTime;
+    }
+
+    public Integer getTotalPosts() {
+        return totalPosts;
+    }
+
+    public void setTotalPosts(Integer totalPosts) {
+        this.totalPosts = totalPosts;
+    }
+
+    public Integer getTotalAcceptedPosts() {
+        return totalAcceptedPosts;
+    }
+
+    public void setTotalAcceptedPosts(Integer totalAcceptedPosts) {
+        this.totalAcceptedPosts = totalAcceptedPosts;
+    }
+
+    public Double getPostsAvgScore() {
+        return postsAvgScore;
+    }
+
+    public void setPostsAvgScore(Double postsAvgScore) {
+        this.postsAvgScore = postsAvgScore;
+    }
+}
