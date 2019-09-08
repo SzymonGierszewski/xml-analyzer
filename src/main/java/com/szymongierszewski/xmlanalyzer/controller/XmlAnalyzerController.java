fa3ff0f5eb4e78f2b36a1a4e9cf68ca5d89c889f
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-09-08 14:28:56 +0200

    add XmlAnalyzerController.java

---
+++ src/main/java/com/szymongierszewski/xmlanalyzer/controller/XmlAnalyzerController.java
@@ -0,0 +1,10 @@
+package com.szymongierszewski.xmlanalyzer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/analyzes")
+public class XmlAnalyzerController {
+
+}
